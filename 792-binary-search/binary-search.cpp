9e07aace458611009156517ba4364527f99e093b
𝐘𝐚𝐬𝐡 𝐊𝐮𝐦𝐚𝐫 <137730575+YashhhCodesHere@users.noreply.github.com>
2025-01-20 11:35:49 +0530

    Time: 4 ms (1.52%) | Memory: 46.5 MB (8.81%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int left = 0, right = nums.size() - 1;
-        while(left <= right){
-            int mid = left + (right - left) / 2;
-            
-            if(nums[mid] == target){
-                return mid;
-            }else if(target < nums[mid]){
-                right = mid - 1;
-            }else{
-                left = mid + 1;
-            }
+    // Here, we'll solve it with recursive approach:-
+    int recursiveBinarySearch(vector<int> nums, int left, int right, int target){
+        if(left > right){
+            return -1;
         }
-        return -1;
+
+        int mid = left + (right - left) / 2;
+        
+        if(nums[mid] == target){
+            return mid;
+        }
+        if(target < nums[mid]){
+            return recursiveBinarySearch(nums, left, (mid - 1), target);
+        }else{
+            return recursiveBinarySearch(nums, (mid + 1), right, target);
+        }
+    }
+    int search(vector<int>& nums, int target) {
+        return recursiveBinarySearch(nums, 0, nums.size() - 1, target);
     }
 };
