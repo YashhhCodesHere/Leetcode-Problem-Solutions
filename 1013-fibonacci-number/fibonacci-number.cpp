112c8b0444fd7bce279b5aa416a37e317fd99b84
𝐘𝐚𝐬𝐡 𝐊𝐮𝐦𝐚𝐫 <137730575+YashhhCodesHere@users.noreply.github.com>
2025-02-07 23:46:42 +0530

    Time: 3 ms (68.05%) | Memory: 7.6 MB (98.36%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -4,6 +4,12 @@ public:
         if(n<=1){
             return n;
         }
-        return fib(n-1) + fib(n-2);
+        int a = 0, b = 1;
+        for(int i = 2; i <= n; i++){
+            int temp = b;
+            b = a+b;
+            a = temp;
+        }
+        return b;
     }
 };
