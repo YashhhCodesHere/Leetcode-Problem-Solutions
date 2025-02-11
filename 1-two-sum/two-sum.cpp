3a9d2edb0df27ed2540c3a977e125384f5b63f02
𝐘𝐚𝐬𝐡 𝐊𝐮𝐦𝐚𝐫 <137730575+YashhhCodesHere@users.noreply.github.com>
2025-02-11 12:10:10 +0530

    Time: 1 ms (82.86%) | Memory: 14.9 MB (35.41%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,15 +1,22 @@
 class Solution {
- public:
-  vector<int> twoSum(vector<int>& nums, int target) {
-    unordered_map<int, int> numToIndex;
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> ump;    // (Element, Index)
+        int n = nums.size();
 
-    for (int i = 0; i < nums.size(); ++i) {
-      if (const auto it = numToIndex.find(target - nums[i]);
-          it != numToIndex.cend())
-        return {it->second, i};
-      numToIndex[nums[i]] = i;
+        for(int i = 0; i < n; i++){
+            int comp = target - nums[i];
+            if(ump.count(comp)){
+                return {ump[comp], i};
+            }
+            ump[nums[i]] = i;
+        }
+        return {};
     }
-
-    throw;
-  }
 };
+
+/*
+    Brute-Force: Using Nested Loops -> O(n^2)
+    Sorting: Using 2-Pointer Approach -> O(nlogn)
+    Optimized: Using Hashing(Unordered Map) -> O(n)
+*/
